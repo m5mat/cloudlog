911ca0e85c28e9680aea2dbd7e7684d62c7d83dc
Ubuntu <matt@docker.rlda2zh2yjcutg45qyegngwr4c.ax.internal.cloudapp.net>
2017-11-08 14:06:37 +0000

    123

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 docker login
-
 cd docker/nginx
 docker build -t cloudlog/nginx .
-
 docker pull nmcteam/php56
-
 docker pull sameersbn/mysql
+docker run -d -p 8080:80 -v $(pwd)/vhost.conf:/etc/nginx/sites-enabled/vhost.conf -v $(pwd)/public:/var/www/public cloudlog/nginx
 
-docker run -d -p 8080:80 -v $(pwd)/vhost.conf:/etc/nginx/sites-enabled/vhost.conf -v $(pwd)/public:/var/www/public cloudlog/nginx;
