0b9c302f9067bfd2c57647dab731d44bb6050eaf
2E1HNK <2e1hnk@deadbeef.cafe>
2017-11-08 14:06:15 +0000

    changed paths

--- docker-build.sh
+++ docker-build.sh
@@ -9,4 +9,4 @@ docker pull nmcteam/php56
 
 docker pull sameersbn/mysql
 
-docker run -d -p 8080:80 -v $(pwd)/vhost.conf:/etc/nginx/sites-enabled/vhost.conf -v $(pwd)/public:/var/www/public cloudlog/nginx;
+docker run -d -p 8080:80 -v $(pwd)/docker/nginx/vhost.conf:/etc/nginx/sites-enabled/vhost.conf -v $(pwd)/src/public:/var/www/public cloudlog/nginx;
